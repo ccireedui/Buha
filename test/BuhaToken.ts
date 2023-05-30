54e42692cb0a3233198b54dcb0e7cf0ba033b1d5
ccireedui <ireedui355@gmail.com>
2023-05-30 10:14:47 +0800

    added signers, upgrading test

--- test/BuhaToken.ts
+++ test/BuhaToken.ts
@@ -11,9 +11,28 @@ describe("BuhaToken", function () {
     }
 
     this.beforeEach(async function () {
+        const accounts = await ethers.getSigners();
+        this.owner = accounts[0];
+        this.otherAccount = accounts[1];
+
         const BuhaToken = await ethers.getContractFactory("BuhaToken");
         this.buhaInstance = await upgrades.deployProxy(BuhaToken);
         await this.buhaInstance.deployed();
+    });
+
+    it("Should get upgraded", async function () {
+        const BuhaTokenV2 = await ethers.getContractFactory("BuhaToken");
+        const buhaInstanceUpgraded = await upgrades.upgradeProxy(this.buhaInstance.address, BuhaTokenV2);
+        expect(await buhaInstanceUpgraded.name()).to.equal("Buha Token");
+    });
+
+    it("Shouldn't be able to upgrade if not owner", async function () {
+        const BuhaTokenV2 = await ethers.getContractFactory("BuhaToken");
+        await expect(upgrades.upgradeProxy(this.buhaInstance.address, BuhaTokenV2.connect(this.otherAccount))).to.be.revertedWith(`AccessControl: account ${this.otherAccount.address.toLowerCase()} is missing role ${await this.buhaInstance.UPGRADER_ROLE()}`);
+    });
+
+    it("Should check if the owner has UPGRADER_ROLE", async function () {
+        expect(await this.buhaInstance.hasRole(await this.buhaInstance.UPGRADER_ROLE(), this.owner.address)).to.equal(true);
     });
 
     it("Should return the right name and symbol", async function () {
